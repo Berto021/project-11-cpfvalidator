df9e0c1cca7fae2c3618f3e4c054ee6382deb559
Humberto <humbertolima108@gmail.com>
2023-05-09 00:01:40 -0300

    colocando mascara de cpf e limitando caracteres para apenas numeros

--- assets/js/validador.js
+++ assets/js/validador.js
@@ -1,3 +1,21 @@
+const input = document.querySelector('.input'); 
+
+input.addEventListener('keypress', () => { 
+    let inputLength = input.value.length
+
+    if( inputLength === 3){
+        input.value += '.'
+    }
+    if( inputLength === 7){
+        input.value += '.'
+    }
+    if( inputLength === 11){
+        input.value += '-'
+    }
+    
+})
+
+
 const cpf = '111.111.111-11'
 const limpaCpf = cpf.replace(/\D+/g,'')//aqui vamos tirar tudo que não é number
 
